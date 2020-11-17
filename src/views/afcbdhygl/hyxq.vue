987f7c0da3842b8618c6373f60e5f7fcd2a8565c
DJL <15570758375@163.com>
2020-11-17 14:06:29 +0800

    commit

--- src/views/afcbdhygl/hyxq.vue
+++ src/views/afcbdhygl/hyxq.vue
@@ -276,7 +276,7 @@
             width="250"
           >
             <template slot-scope="scope">
-              <span>{{ scope.row.cardname }}({{scope.row.cardno}})</span>
+              <span>{{ scope.row.cardname }}({{scope.row.cardno.substring()}})</span>
             </template>
           </el-table-column>
 
